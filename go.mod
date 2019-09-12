58abd5827ae516c4894505d43a6a08803dd087ab
Fulvio Carrus <fcarrus@redhat.com>
2019-09-12 17:41:47 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/fcarrus/gokubetest
+
+go 1.12
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/googleapis/gnostic v0.3.1 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/openshift/api v3.9.1-0.20190322043348-8741ff068a47+incompatible
+	github.com/openshift/client-go v0.0.0-20180830153425-431ec9a26e50
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.0.0-20180712090710-2d6f90ab1293
+	k8s.io/apimachinery v0.0.0-20190223001710-c182ff3b9841
+	k8s.io/client-go v0.0.0-20190117233410-4022682532b3
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Note: the example only works with the code within the same release/branch.
+package main
+
+import (
+	// "flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	// "path/filepath"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var clientset *kubernetes.Clientset
+
+var namespace = "prova"
+
+var config *rest.Config
+
+var address, port string
+
+func main() {
+	// creates the in-cluster config
+	var err error
+	config, err = rest.InClusterConfig()
+	var kcfg clientcmd.ClientConfig
+	if err != nil {
+		kcfg = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			clientcmd.NewDefaultClientConfigLoadingRules(),
+			&clientcmd.ConfigOverrides{},
+		)
+
+		config, err = kcfg.ClientConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println("Using external Config")
+		namespace, _, err = kcfg.Namespace()
+
+		// Listen only on localhost when not running in a Pod
+		address = "localhost"
+
+	} else {
+
+		fmt.Println("Using in-cluster config")
+		ns, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+		if err != nil {
+			fmt.Println("ERR: ", err)
+		} else {
+			namespace = string(ns)
+		}
+
+		// Listen on ANY when inside a Pod
+		address = "0.0.0.0"
+
+	}
+
+	// creates the clientset
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Current namespace: ", namespace)
+
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	go webServer()
+	go showPods()
+
+	fmt.Println("Starting main loop.")
+	for {
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func showPods() {
+
+	for {
+		pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{
+			LabelSelector: "app=ocp-ovs-nodecheck",
+			// LabelSelector: "app=phpinfo",
+		})
+
+		for _, pod := range pods.Items {
+			fmt.Printf("Pod: name:%v state:%v ip:%v\n", pod.Name, pod.Status.Phase, pod.Status.PodIP)
+			if pod.Status.Phase == "Running" {
+				url := fmt.Sprintf(
+					"http://%v:%v/",
+					pod.Status.PodIP,
+					pod.Spec.Containers[0].Ports[0].ContainerPort)
+				fmt.Printf("Attempting to GET %s ...", url)
+				resp, err := http.Get(url)
+				if err != nil {
+					fmt.Println("ERR: ", err)
+				} else {
+					fmt.Println("INF: ", resp)
+				}
+
+			}
+			fmt.Println("")
+		}
+
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the namespace\n\n", len(pods.Items))
+
+		time.Sleep(5 * time.Second)
+	}
+
+}
+
+func webServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "It works.\n")
+	})
+	url := fmt.Sprintf("%s:%s", address, port)
+	fmt.Printf("Starting web server on %s ...\n", url)
+	http.ListenAndServe(url, nil)
+}
